208827db07ecd08f1f16bc0ba6158aa4c3430db2
Prudhvi <prudhvimalladi667@outlook.com>
2024-11-12 11:43:23 +0530

    Added code

--- src/LambdaExpression.java
+++ src/LambdaExpression.java
@@ -1,2 +1,15 @@
+
+interface FunInterface{
+    void fun(int x);
+    default void heavyFun(){
+        System.out.println("Hello");
+    }
+}
+
 public class LambdaExpression   {
+    public static void main(String[] args) {
+        FunInterface fobj = (int x) -> System.out.println(2+x);
+        fobj.fun(5);
+        fobj.heavyFun();
+    }
 }
